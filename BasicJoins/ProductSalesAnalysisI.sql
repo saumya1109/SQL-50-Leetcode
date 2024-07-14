8aefd7ac134ba2ead8c9cdc55cda5564fca9b545
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-14 15:13:38 -0400

    Updating query

--- BasicJoins/ProductSalesAnalysisI.sql
+++ BasicJoins/ProductSalesAnalysisI.sql
@@ -3,3 +3,10 @@
 select p.product_name,s.year,s.price
 From Sales s join Product p on 
 s.product_id= p.product_id;
+
+
+#Another way to solve this is using "Using function"
+select p.product_name,s.year,s.price
+From Sales s 
+join Product p 
+using (product_id);
