3cc39ef50929f72f0a3bcfb6cd97a24b3c636417
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-15 16:05:01 -0400

    Create MonthlyTransactionsI.sql

---
+++ BasicAggregateFunctions/MonthlyTransactionsI.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+
+select DATE_FORMAT(trans_date,'%Y-%m') as month
+, country
+,count(id) as trans_count
+,SUM(state='approved') as approved_count
+,SUM(amount) as trans_total_amount
+,SUM(IF (state='approved', amount, 0)) as approved_total_amount
+from Transactions
+group by month,country
