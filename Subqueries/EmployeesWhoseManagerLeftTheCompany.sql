c73955ca418cab11242f160eca19fbe7e7e1b4c2
Saumya Gupta <saumya.gupta1109@gmail.com>
2024-07-17 00:07:37 -0400

    Create EmployeesWhoseManagerLeftTheCompany.sql

---
+++ Subqueries/EmployeesWhoseManagerLeftTheCompany.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select employee_id 
+from Employees
+where manager_id not in (Select distinct employee_id from Employees )
+and salary < 30000
+order by employee_id
